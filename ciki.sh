135daae67d1754a1ecade8b96ef522ca7b297d6b
oleum0 <autisticretard012@proton.me>
2023-06-21 18:09:26 +0530

    fixed an error and added usage section in readme

--- ciki.sh
+++ ciki.sh
@@ -7,4 +7,4 @@ query=${query^}
 title=$(curl -s https://en.wikipedia.org/wiki/$query | grep mw-page-title-main | cut -d'>' -f3 | cut -d'<' -f1)
 # echo "$title"
 echo -e "\033[1m$title\033[0m"
-curl -s https://en.wikipedia.org/wiki/$query | grep "<p>" | sed -e 's/<[^>]*>//g' -e 's/\&\#[0-9][0-9]//g' -e 's/\;[0-9][0-9]\;//g' | head -3
+curl -s https://en.wikipedia.org/wiki/$query | grep "<p>" | sed -e 's/<[^>]*>//g' -e 's/\&\#[0-9][0-9]//g' -e 's/\;[0-9][0-9]\;//g'  -e 's/\;//g' | head -1
